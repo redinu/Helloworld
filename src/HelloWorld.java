aa0e55737a0df45cdbc4ea72e35b22baff749376
Rediet Ayalew <rediet.d.ayalew@gmail.com>
2017-05-24 13:25:29 -0400

    first commit of first project

---
+++ src/HelloWorld.java
@@ -0,0 +1,14 @@
+
+public class HelloWorld {
+
+	/* this is a comment
+	 * Autor : rediet
+	 * Date : 5/24/2017
+	 * 
+	 */
+	public static void main(String[] args) {
+		
+		System.out.println("Hello world");
+	}
+
+}
